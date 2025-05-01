ed2ac7647cd097dbf527a58e6db0e32147c126c6
raidenh <raidenh@uw.edu>
2025-05-01 11:04:38 -0700

    added /test/ package for testing our program

---
+++ test/TestModel.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Class for testing classes and methods of the game model
+ * @author Raiden H
+ * @version May 1, 2025
+ */
+public class TestModel {
+    //stub class to implement tests later
+}
