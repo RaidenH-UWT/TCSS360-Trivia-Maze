89b98b61d12868603b93a800d3a651fe216502be
raidenh <raidenh@uw.edu>
2025-05-01 10:07:33 -0700

    added documentation to the enums

--- model/Direction.java
+++ model/Direction.java
@@ -1,5 +1,10 @@
 package model;
 
+/**
+ * Enumeration of the four cardinal directions.
+ * @author Raiden H
+ * @version May 1, 2025
+ */
 public enum Direction {
     NORTH,
     SOUTH,
--- model/QuestionType.java
+++ model/QuestionType.java
@@ -1,5 +1,10 @@
 package model;
 
+/**
+ * Enumeration of the 3 question types.
+ * @author Raiden H
+ * @version May 1, 2025
+ */
 public enum QuestionType {
     MULTIPLE_CHOICE,
     TRUE_FALSE,
