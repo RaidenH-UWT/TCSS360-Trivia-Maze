4daa02134dc8f018a3950f553c2dba0680b72f69
raidenh <raidenh@uw.edu>
2025-05-01 11:09:57 -0700

    TODO item for saving

--- src/model/GameSaver.java
+++ src/model/GameSaver.java
@@ -1,5 +1,8 @@
 package src.model;
 
+// TODO: Decide whether we want to use Serialization (security flaws, not human readable)
+// or read/write from file (takes time to implement I/O pattern and structure)
+
 public class GameSaver {
     // Potentionally add a List<File> or List<String> of all saves?
 
