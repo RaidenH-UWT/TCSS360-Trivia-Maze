4ab23829e390311a1a6b4afae3e44603f34235ba
RaidenH-UWT <raidenh@uw.edu>
2025-05-05 17:04:41 -0700

    test for the Maze class

--- test/TestMaze.java
+++ test/TestMaze.java
@@ -1,10 +1,82 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import src.model.Maze;
+import src.model.Room;
+
 /**
- * Class for testing the Maze object and its contained Rooms and Doors.
+ * Class for testing the Maze object.
  * @author Raiden H
  * @version May 2, 2025
  */
 public class TestMaze {
+    /**
+     * Objects for testing on.
+     */
+    Maze testMaze;
+
+    /**
+     * Setup clean Maze object before each test.
+     */
+    @BeforeEach
+    void setup() {
+        testMaze = new Maze(5, 5);
+    }
+
+    /**
+     * Test the Maze contructor, making sure appropriate exceptions are thrown.
+     */
+    @Test
+    void testConstructor() {
+        assertThrows(IllegalArgumentException.class, () -> testMaze = new Maze(3, 0));
+        assertThrows(IllegalArgumentException.class, () -> testMaze = new Maze(0, 3));
+        assertThrows(IllegalArgumentException.class, () -> testMaze = new Maze(0, 0));
+    }
+
+    /**
+     * Test dimension getters.
+     */
+    @Test
+    void testDimensions() {
+        assertEquals(5, testMaze.getWidth());
+        assertEquals(5, testMaze.getHeight());
+
+        testMaze = new Maze(3, 7);
+
+        assertEquals(3, testMaze.getWidth());
+        assertEquals(7, testMaze.getHeight());
+    }
+
+    /**
+     * Test the isPathAvailable() method.
+     */
+    @Test
+    void testIsPathAvailable() {
+        // Please implement, unsure what the intended behaviour is.
+    }
     
+    /**
+     * Test the getRoom() method.
+     */
+    @Test
+    void testGetRoom() {
+        // Consider setting up a mock Maze object for testing.
+
+        // Testing bounds allowed
+        assertThrows(IndexOutOfBoundsException.class, () ->  testMaze.getRoom(-1, 0));
+        assertThrows(IndexOutOfBoundsException.class, () ->  testMaze.getRoom(0, -1));
+        assertThrows(IndexOutOfBoundsException.class, () ->  testMaze.getRoom(6, 0));
+        assertThrows(IndexOutOfBoundsException.class, () ->  testMaze.getRoom(0, 6));
+
+        // Finally testing the returned object
+        assertEquals(new Room(0, 0), testMaze.getRoom(0, 0));
+        // TODO: with a mock object test this case more thoroughly
+    }
+
+    // TODO: implement tests for entrance/exit getters once those are set, and isCompleted()
 }
