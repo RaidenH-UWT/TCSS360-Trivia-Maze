d6bcc0734f00d8a8c0a846606a8f69c7e3b83d0a
raidenh <raidenh@uw.edu>
2025-06-09 18:10:25 -0700

    stub SettingsPage class

---
+++ src/view/SettingsPage.java
@@ -0,0 +1,12 @@
+package src.view;
+
+import javax.swing.JFrame;
+
+/**
+ * [ DESCRIPTION HERE ]
+ * @author Kalen Cha
+ * @version Spring 2025
+ */
+public class SettingsPage extends JFrame {
+    
+}
